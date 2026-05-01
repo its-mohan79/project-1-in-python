efde63d41e515ffe7955cb530d0df69f7166834f
its-mohan79 <mohanroy8989@gmail.com>
2026-05-01 17:17:20 +0530

    today i learn previose topic in java

--- snake water gun.py
+++ snake water gun.py
@@ -5,7 +5,7 @@ import random
 -1 for  water
 0 for gun
 '''
-computer=random.choice([-1,0000,1])
+computer=random.choice([-1,000,1])
 youstr= (input("enter you choice: "))
 youdict={"s": 1,"w": -1,"g": 0}
 reversedict={1 : "snake", -1 : "water", 0 : "gun"}
